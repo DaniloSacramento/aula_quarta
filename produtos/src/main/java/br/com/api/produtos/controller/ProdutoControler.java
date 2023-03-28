7e27ed893ed5ce23ca7d4b127f2a61035ad480f2
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2023-03-27 23:29:07 -0300

    01

---
+++ produtos/src/main/java/br/com/api/produtos/controller/ProdutoControler.java
@@ -0,0 +1,43 @@
+package br.com.api.produtos.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.models.ProdutoModelo;
+import br.com.api.produtos.models.RespostaModelo;
+import br.com.api.produtos.services.produto_service;
+
+@RestController 
+public class ProdutoControler {
+    
+
+    @Autowired 
+    private produto_service ps; 
+    
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){ 
+        return ps.listar(); 
+    }
+    @GetMapping("/")
+    public String rota(){
+        return "API de produtos funcionando";
+    }
+}
